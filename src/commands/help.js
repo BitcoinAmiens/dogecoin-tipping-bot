a2eccddfbc7c3b7ace2c9df0a947ccedc73972b8
Louis Viart <louis@iteracode.fr>
2018-01-13 14:55:58 +0100

    Add Withdraw command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,10 +1,12 @@
-const HELP_TEXT = '```Wow. DogeCoin to moon. \n \
+const HELP_TEXT =
+  '```Wow. DogeCoin to moon. \n \
 Command list : \n \
   - **help** : Get help ! \n \
   - **tip** : Tip someone some dogecoin \n \
   - **balance** : Get your balance \n \
   - **rate** : Get current value \n \
   - **address** : Show an used wallet so you can refill your wallet \n \
+  - **withdraw** : Move coins to an external address \n \
 ```'
 
 function help (message) {
